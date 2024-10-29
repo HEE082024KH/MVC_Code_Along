ad834ecd0271f136d0c0340ba94323a53ef85611
hee082024kh <hee082024@gmail.com>
2024-10-29 10:52:18 +0100

    Use controller to update the model

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ class Program
         View view = new();
         Controller controller = new(model, view);
 
+        // Use controller to test the program.
         controller.DisplayModel();
+        // Check if controller mehtods work
+        controller.UpdateName();
+        controller.UpdateAge();
+        // Display the model after it was updated
+        controller.DisplayModel();
+        Console.ReadLine();
     }
 }
